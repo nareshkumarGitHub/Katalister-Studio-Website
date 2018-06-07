c43fe22a9663b364fffd1dbdf81e9cdebfd15da5
nareshkumarGitHub <naresh.wd@gmail.com>
2018-06-07 16:23:12 +0530

    Intialized Katalister
    Katalister Studio Website

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+jQuery(document).ready(function( $ ) {
+
+
+  // Preloader
+  $(".loading").fadeOut(500);
+
+  "use strict";
+
+  var wind = $(window);
+
+  // scrollIt
+  $.scrollIt({
+    upKey: 38,                // key code to navigate to the next section
+    downKey: 40,              // key code to navigate to the previous section
+    easing: 'swing',          // the easing function for animation
+    scrollTime: 600,          // how long (in ms) the animation takes
+    activeClass: 'active',    // class given to the active nav element
+    onPageChange: null,       // function(pageIndex) that is called when page is changed
+    topOffset: -88            // offste (in px) for fixed top navigation
+  });
+
+
+
+  // navbar scrolling background
+  wind.on("scroll",function () {
+
+      var bodyScroll = wind.scrollTop(),
+          navbar = $(".navbar");
+
+      if(bodyScroll > 100){
+
+          navbar.addClass("nav-scroll");
+
+      }else{
+
+          navbar.removeClass("nav-scroll");
+      }
+  });
+
+
+/*// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });*/
+  
+  // Back to top button
+  $(window).scroll(function() {
+
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+      
+  });
+  
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+      return false;
+  });
+
+});
+
+
+$(document).ready(function(){
+  var wind = $(window);
+  var bodyScroll = wind.scrollTop(),
+      navbar = $(".navbar");
+
+  if(bodyScroll > 100){
+
+      navbar.addClass("nav-scroll");
+
+  }else{
+
+      navbar.removeClass("nav-scroll");
+  }
+})
---
+++ js/custom_new.js
@@ -0,0 +1,114 @@
+jQuery(document).ready(function( $ ) {
+
+
+  // Preloader
+  $(".loading").fadeOut(500);
+
+  "use strict";
+
+  var wind = $(window);
+  var bodyScroll = wind.scrollTop();
+  var previousScrollPoint = bodyScroll;
+
+  // navbar scrolling background
+  wind.on("scroll",function () {
+
+    var bodyScroll = wind.scrollTop(),
+    navbar = $(".navbar");
+    if (previousScrollPoint > bodyScroll) {
+      navbar.removeClass("nav-scroll-in").addClass("nav-scroll");
+    } else {
+      navbar.addClass("nav-scroll-in").removeClass("nav-scroll");
+    }
+
+    if (bodyScroll == 0) {
+      navbar.removeClass("nav-scroll").removeClass("nav-scroll-in"); 
+    } 
+    previousScrollPoint = bodyScroll;
+      
+  });
+
+
+/*// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });*/
+  
+  // Back to top button
+  $(window).scroll(function() {
+
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+      
+  });
+  
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+      return false;
+  });
+
+});
+
+
+
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("body a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
